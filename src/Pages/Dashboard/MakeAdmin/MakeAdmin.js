a3a939b081ae782030d046195341431a64923146
rakibtweets <rakibhasanroky0@gmail.com>
2021-11-07 04:58:54 +0600

    adding user to admin role

--- src/Pages/Dashboard/MakeAdmin/MakeAdmin.js
+++ src/Pages/Dashboard/MakeAdmin/MakeAdmin.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import { Button, TextField } from '@mui/material';
+import React, { useState } from 'react';
 
 const MakeAdmin = () => {
+  const [email, setEmail] = useState('');
+
+  const handleOnBlur = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleAdminSubmit = (e) => {
+    const user = { email };
+    fetch('http://localhost:5000/users/admin', {
+      method: 'PUT',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+
+    e.preventDefault();
+    e.target.reset();
+  };
   return (
     <div>
       <h3>Making Admin</h3>
+      <form onSubmit={handleAdminSubmit}>
+        <TextField
+          sx={{ width: '50%' }}
+          id="outlined-required"
+          label="Email"
+          type="email"
+          onBlur={handleOnBlur}
+        />
+        <Button
+          sx={{ py: 2, px: 4, bgcolor: '#19D3AE' }}
+          variant="contained"
+          type="submit"
+        >
+          Make Admin
+        </Button>
+      </form>
     </div>
   );
 };
